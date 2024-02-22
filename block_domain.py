965ea078998710f4a5aba7f10c64180b2ba5a4b9
grant <grantrose1@gmail.com>
2024-02-22 16:07:08 -0500

    domain blocking testing

--- block_domain.py
+++ block_domain.py
@@ -5,7 +5,6 @@
 import requests
 import json
 import sqlite3
-import re
 
 # Connecting to sqlite
 conn = sqlite3.connect('Sqlite3.db')
@@ -17,10 +16,26 @@ cursor = conn.cursor()
 ver = conn.execute('SELECT Version FROM Instances')
 for row in ver:
     a = ver.fetchone()
-    c = str(a)
-    d = c[2:7]
-    #e = float(d)
-    print(d)
-    if d != '4.2.0':
-        print('yes')
+    st = a[0]
+    print(st)
+    num = float((st[:-2]))
+    print(num)
+    if num < 4.2:
+        dn = conn.execute('SELECT Domain FROM Instances Where Version="%s" ' % (st))
+        d = dn.fetchone()
+        dm = d[0]
+        print(d[0])
+        url = "https://{'instance url'}/api/v1/admin/domain_blocks"
+        payload = json.dumps({
+        "domain": dm
+        })
+        headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer '
+        }
+        response = requests.request("POST", url, headers=headers, data=payload)
+
+        print(response.text)
+        
+
 conn.close()
