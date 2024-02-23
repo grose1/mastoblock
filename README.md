58e00eee73adfc2c4d72d04430996ef59ba9fc55
grant <grantrose1@gmail.com>
2024-02-23 09:35:05 -0500

    set up automatic blocking via api and updated readme

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # mastoblock
+### A python program aimed at automating the process of blocking instances with out of date and vulnerable software versions from federating with your server. 
+
+It creates a local database of instances you federate with and their software versions. It then uses that database to automatically block those instances with out of date software. 
+The id number for the domain blocks returned from your mastodon server are then logged in the database so that they can be unblocked via the api at a later date if need be. 
+currently the software version to block is set at anything less than 4.2
+
+
 
 1. Run the `create_database.py` script to create your sqlite database and table. You only need to call this once.
 2. Create a  `.env` file (see `.env.example` file) replace `instance_url` in the `server_url` field with the domain of your instance.
--- block_domain.py
+++ block_domain.py
@@ -1,10 +1,25 @@
-# Read version number from database and create a domain block on versions less than x.x.x
+# Read version number from database and create a domain block on versions less than x.x.x 
+# Then saves the id number returned for the domain block from the mastodon server to the database
+# This is currently set to block any instance with a version number less than 4.2
 
 # WORK IN PROGRESS JUST IGNORE FOR NOW ;)
 
 import requests
 import json
 import sqlite3
+from configparser import ConfigParser
+
+
+
+# Read env file
+config_object = ConfigParser()
+config_object.read(".env")
+serverenv = config_object['SERVER']
+server = serverenv['server_url']
+token = serverenv['api_token']
+
+bearer = 'Bearer ' + token
+
 
 # Connecting to sqlite
 conn = sqlite3.connect('Sqlite3.db')
@@ -17,25 +32,34 @@ ver = conn.execute('SELECT Version FROM Instances')
 for row in ver:
     a = ver.fetchone()
     st = a[0]
-    print(st)
+    #print(st)
     num = float((st[:-2]))
-    print(num)
+    #print(num)
     if num < 4.2:
         dn = conn.execute('SELECT Domain FROM Instances Where Version="%s" ' % (st))
         d = dn.fetchone()
         dm = d[0]
-        print(d[0])
-        url = "https://{'instance url'}/api/v1/admin/domain_blocks"
+        #print(d[0])
+        url = "https://{}/api/v1/admin/domain_blocks".format(server)
         payload = json.dumps({
-        "domain": dm
+        "domain": dm ,
+        "severity": "suspend" ,
+        "public_comment": "Software out of Date" ,
+        "reject_media": 'true' ,
+        "reject_reports": 'true' ,
+        "obfuscate": 'true' 
         })
         headers = {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer '
+        'Authorization': bearer
         }
         response = requests.request("POST", url, headers=headers, data=payload)
-
-        print(response.text)
+        data = response.json()
+        bl_id = data['id']
+        print(bl_id)
+        cursor.execute('UPDATE Instances set BL_ID = ? where Domain= ? ', (bl_id, dm))
+        conn.commit()
+        
         
 
 conn.close()
